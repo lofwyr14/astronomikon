5dae8244382d757676efc807e9fc4cdea9319472
Udo Schnurpfeil <udo.schnurpfeil@irian.eu>
2022-02-16 07:42:04 +0100

    chore: update lit

--- www/js/astro-image.js
+++ www/js/astro-image.js
@@ -1,5 +1,5 @@
-import { html, render } from "lit-html";
-import { ifDefined } from "lit-html/directives/if-defined";
+import { html, render } from "lit";
+import { ifDefined } from "lit/directives/if-defined.js";
 class AstroImage {
     constructor(image) {
         this.id = image.id;
